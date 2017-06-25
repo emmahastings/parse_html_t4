1d9814e6b4e323b6fdbb2d9bd934b96db1c8d130
emmahastings <emmakhastings@gmail.com>
2017-06-25 12:46:01 +0100

    Move URL to config file rather than hardcode

---
+++ src/main/java/com/hastings/model/YamlConfig.java
@@ -0,0 +1,15 @@
+package com.hastings.model;
+
+/**
+ * Created by emmakhastings on 25/06/2017.
+ * <p>
+ * Domain object to hold config file attributes
+ */
+public class YamlConfig {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+}
